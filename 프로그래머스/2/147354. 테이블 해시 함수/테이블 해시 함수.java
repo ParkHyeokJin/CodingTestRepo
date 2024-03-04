6a83704e30b1e5c460c16ea706aefa9bdd605698
박혁진 <parkhjpp@daum.net>
2024-03-05 07:58:25 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.87 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+
+        Arrays.sort(data, (d1, d2) -> {
+            if (d1[col - 1] == d2[col - 1]) {
+                return Integer.compare(d2[0], d1[0]);
+            }
+            return Integer.compare(d1[col - 1], d2[col - 1]);
+        });
+
+        for(int i = row_begin - 1; i < row_end; i++){
+            int sum = 0;
+            for(int j = 0; j < data[i].length; j++){
+                sum += data[i][j] % (i + 1);
+            }
+            answer ^= sum;
+        }
+        
+        return answer;
+    }
+}
